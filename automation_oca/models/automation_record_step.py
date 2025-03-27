aa723d119743f69a4a830ff23728493473e2a496
Enric Tobella <enric.tobella@dixmit.com>
2025-03-27 06:23:55 +0100

    [IMP] automation_oca: Enforce verification domain on activities

--- automation_oca/models/automation_record_step.py
+++ automation_oca/models/automation_record_step.py
@@ -9,6 +9,7 @@ import werkzeug.urls
 from dateutil.relativedelta import relativedelta
 
 from odoo import _, api, fields, models, tools
+from odoo.exceptions import ValidationError
 from odoo.tools.safe_eval import safe_eval
 
 
@@ -328,6 +329,17 @@ class AutomationRecordStep(models.Model):
         todo._trigger_activities()
 
     def _set_activity_done(self):
+        domain = safe_eval(
+            self.configuration_step_id.activity_verification_domain or "[]",
+            self.configuration_step_id.configuration_id._get_eval_context(),
+        )
+        if domain and not self.record_id.resource_ref.filtered_domain(domain):
+            raise ValidationError(
+                _(
+                    "The record does not fulfill the expected domain:\n%(domain)s",
+                    domain=self.configuration_step_id.activity_verification_domain_error,
+                )
+            )
         self.write({"activity_done_on": fields.Datetime.now()})
         self.child_ids.filtered(
             lambda r: r.trigger_type == "activity_done"
